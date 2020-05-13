1c21d0c3ccab4ed835a60314aa07b5947125fea5
David Moftakhar <david@sellmysnaps.com>
2020-05-13 10:28:33 +0100

    Post model and query created.

---
+++ php_rest_myblog/models/Post.php
@@ -0,0 +1,40 @@
+<?php 
+
+    class Post {
+        // DB properties
+        private $conn;
+        private $table = 'posts';
+
+        // Post properties
+        public $id;
+        public $category_id;
+        public $category_name;
+        public $title;
+        public $body;
+        public $author;
+        public $created_at;
+
+        // Constructor with DB
+        public function __construct($db) {
+            $this->conn = $db;
+        }
+
+        // Get Posts Method
+        public function read(){
+            // Create query
+            $query = 'SELECT 
+                    c.name as category_name,
+                    p.id,
+                    p.category_id,
+                    p.title,
+                    p.body,
+                    p.author,
+                    p.created_at
+                FROM 
+                    ' . $this->table . ' p
+                LEFT JOIN
+                    categories c ON p.category_id = c.id
+                ORDER BY
+                    p.created_at DESC';
+        }
+    }
