ae582218d4e6f05d6cf3cbc5d3179211a71068cf
David Moftakhar <david@sellmysnaps.com>
2020-05-13 09:58:27 +0100

    DB class complete.

--- php_rest_myblog/config/Database.php
+++ php_rest_myblog/config/Database.php
@@ -13,10 +13,14 @@
             $this->conn = null;
 
             try{
-                    
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname' . $this->db_name, 
+                $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRORMODE_EXCEPTION);
             } catch(PDOException $e) {
-
+                echo 'Connect Error: ' . $e->getMessage();
             }
+
+            return $this->conn;
         }
     }
     
