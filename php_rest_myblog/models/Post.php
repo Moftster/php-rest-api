7e135b7f11b13bc0af7c7053fc09ba46c6330e2c
David Moftakhar <david@sellmysnaps.com>
2020-05-14 13:56:20 +0100

    Read single method completed.

--- php_rest_myblog/models/Post.php
+++ php_rest_myblog/models/Post.php
@@ -1,28 +1,31 @@
-<?php 
+<?php
 
-    class Post {
-        // DB properties
-        private $conn;
-        private $table = 'posts';
+class Post
+{
+    // DB properties
+    private $conn;
+    private $table = 'posts';
 
-        // Post properties
-        public $id;
-        public $category_id;
-        public $category_name;
-        public $title;
-        public $body;
-        public $author;
-        public $created_at;
+    // Post properties
+    public $id;
+    public $category_id;
+    public $category_name;
+    public $title;
+    public $body;
+    public $author;
+    public $created_at;
 
-        // Constructor with DB
-        public function __construct($db) {
-            $this->conn = $db;
-        }
+    // Constructor with DB
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
 
-        // Get Posts Method
-        public function read(){
-            // Create query
-            $query = 'SELECT 
+    // Get Posts Method
+    public function read()
+    {
+        // Create query
+        $query = 'SELECT 
                     c.name as category_name,
                     p.id,
                     p.category_id,
@@ -37,11 +40,50 @@
                 ORDER BY
                     p.created_at DESC';
 
-            // Prepare statement
-            $stmt = $this->conn->prepare($query);
+        // Prepare statement
+        $stmt = $this->conn->prepare($query);
 
-            // Execute query
-            $stmt->execute();
-            return $stmt;
-        }
+        // Execute query
+        $stmt->execute();
+        return $stmt;
     }
+
+    // Get single post
+    public function read_single()
+    {
+    // Create query
+    $query = 'SELECT 
+                    c.name as category_name,
+                    p.id,
+                    p.category_id,
+                    p.title,
+                    p.body,
+                    p.author,
+                    p.created_at
+                FROM 
+                    ' . $this->table . ' p
+                LEFT JOIN
+                    categories c ON p.category_id = c.id
+                WHERE
+                    p.id = ?
+                LIMIT
+                    0,1';
+
+    // Prepare statement
+    $stmt = $this->conn->prepare($query);
+
+    // Bind ID
+    $stmt->bindParam(1, $this->id);
+
+    // Execute query
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // Set properties
+    $this->title = $row['title'];
+    $this->body = $row['body'];
+    $this->author = $row['author'];
+    $this->category_id = $row['category_id'];
+    $this->category_name = $row['category_name'];
+}
+}
